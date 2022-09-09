0d0a7dddaf40c692d05cacbc3d0e2eeeab4b3e27
Unpei2 <r.ho5@share.epsb.ca>
2022-09-09 14:10:28 -0600

    done

--- main.js
+++ main.js
@@ -11,44 +11,24 @@ function btnClicked() {
   let d2Limit = +document.getElementById('d2-limit').value;
   let d3Speed = +document.getElementById('d3-speed').value;
   let d3Limit = +document.getElementById('d3-limit').value;
-
+  
   // Calculate & Output Ticket Types
   // Driver 1
-  let d1Ticket;
-  if (d1Speed > d1Limit + 40) {
-    d1Ticket = 'Really Big Ticket';
-  } else if (d1Speed > d1Limit + 20) {
-    d1Ticket = 'Big Ticket';
-  } else if (d1Speed > d1Limit) {
-    d1Ticket = 'Small Ticket';
-  } else {
-    d1Ticket = 'No Ticket';
-  }
-  document.getElementById('d1-ticket').innerHTML = d1Ticket;
-
+  document.getElementById('d1-ticket').innerHTML = checkTicket(d1Speed,d1Limit);
   // Driver 2
-  let d2Ticket;
-  if (d2Speed > d2Limit + 40) {
-    d2Ticket = 'Really Big Ticket';
-  } else if (d2Speed > d2Limit + 20) {
-    d2Ticket = 'Big Ticket';
-  } else if (d2Speed > d2Limit) {
-    d2Ticket = 'Small Ticket';
-  } else {
-    d2Ticket = 'No Ticket';
-  }
-  document.getElementById('d2-ticket').innerHTML = d2Ticket;
-
+  document.getElementById('d2-ticket').innerHTML = checkTicket(d2Speed,d2Limit);
   // Driver 3
-  let d3Ticket;
-  if (d3Speed > d3Limit + 40) {
-    d3Ticket = 'Really Big Ticket';
-  } else if (d3Speed > d3Limit + 20) {
-    d3Ticket = 'Big Ticket';
-  } else if (d3Speed > d3Limit) {
-    d3Ticket = 'Small Ticket';
-  } else {
-    d3Ticket = 'No Ticket';
+  document.getElementById('d3-ticket').innerHTML = checkTicket(d3Speed,d3Limit);
+}
+function checkTicket (speed, limit){
+  if (speed - limit > 40){
+    ticket = "Really Big Ticket"
+  } else if (speed - limit > 20){
+    ticket = "Big ticket"
+  } else if (speed > limit){
+    ticket = "Ticket"
+  } else{
+    ticket = "No Ticket "
   }
-  document.getElementById('d3-ticket').innerHTML = d3Ticket;
+  return ticket
 }
